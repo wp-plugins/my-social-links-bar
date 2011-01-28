bd6ea00c9381cc88b02b280cc9732dd06afba336
fdias.webdev <fdias.webdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-28 23:16:53 +0000

    Version 0.9 bug fixed during load wp page. jquery conflict when is using $
    git-svn-id: https://plugins.svn.wordpress.org/my-social-links-bar/trunk@338378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mySocialLinksBar.php
+++ mySocialLinksBar.php
@@ -3,7 +3,7 @@
 Plugin Name: My Social Links Bar
 Plugin URI: http://flechaweb.com.br
 Description: An WP Plugin that show/hidde a toolbar with social links of the wp blog/site owner 
-Version: 0.8
+Version: 0.9
 Author: Flechaweb Developing 
 Author URI: http://www.flechaweb.com.br
 License: GPL2
